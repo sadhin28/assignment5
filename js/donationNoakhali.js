1895e0350e77661512892e74bf8f8b0c3ff09493
sadhin28 <taosifsadhin113@gmail.com>
2025-04-13 19:14:20 +0600

    add a function

--- js/donationNoakhali.js
+++ js/donationNoakhali.js
@@ -2,6 +2,7 @@ document.getElementById('DonateNow').addEventListener('click',function(){
     const amount = getvalueByid('DonationAmount')
    
    const fixedamount = getTextvalueByid('FixedBlance')
+   
    if(isNaN(amount)){
     alert('Only money is donated here.');
     return;
@@ -10,7 +11,9 @@ document.getElementById('DonateNow').addEventListener('click',function(){
         const Dicrimentamount=fixedamount-amount;
         console.log(Dicrimentamount)
         document.getElementById('FixedBlance').innerHTML=Dicrimentamount;
-        
+        const updateblance = getTextvalueByid('Updateblance');
+        const TotalBlance =amount+updateblance
+        document.getElementById('Updateblance').innerText=TotalBlance;
     }
     else{
         alert('There is not enough money in the fund. You have '+ fixedamount+' taka in your account.' )
