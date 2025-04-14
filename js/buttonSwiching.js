1388a22729a39590f5cbac0793b291271edf20ca
sadhin28 <taosifsadhin113@gmail.com>
2025-04-14 14:53:23 +0600

    add foooter section

--- js/buttonSwiching.js
+++ js/buttonSwiching.js
@@ -10,13 +10,14 @@ function showPage(pageId) {
     document.getElementById(id).classList.add('hidden');
     document.getElementById(id + 'Btn').classList.remove('bg-green-500');
     document.getElementById(id + 'Btn').classList.add('bg-gray-500');
-    
+   
 
   });
 
   document.getElementById(pageId).classList.remove('hidden');
   document.getElementById(pageId + 'Btn').classList.remove('bg-gray-500');
   document.getElementById(pageId + 'Btn').classList.add('bg-green-500');
+  
 }
 
 showPage('donation');
